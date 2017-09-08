357c7efc979a13e0776a0580dedffff8a46e1790
shibinhameed007 <shibinhameed007@gmail.com>
2017-09-08 20:25:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile('article one req and will be served here');
 });
 
-app.get('article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile('article two req and will be served here');
 });
 
-app.get('article-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.sendFile('article three req and will be served here');
 });
 
